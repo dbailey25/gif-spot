d9491717008ff37c8e4e1b5d37b41dccff26af6c
dbailey25 <dtbailey25@gmail.com>
2018-01-04 16:36:45 -0500

    add new button

--- assets/javascript/gifSpot.js
+++ assets/javascript/gifSpot.js
@@ -1,43 +1,58 @@
-$(document).ready(function(){
-  // global variables
-  var buttons = ['Chevy Corvette','Chevy Camaro','Ford Mustang','Aston Martin','Koenigsegg','Nascar Race Car','Le Mans Race Car', 'Land Speed Race Car'];
+$(document).ready(function() {
+      // global variables
+      var buttons = ['Chevy Corvette', 'Chevy Camaro', 'Ford Mustang', 'Aston Martin', 'Koenigsegg', 'Nascar Race Car', 'Le Mans Race Car', 'Land Speed Race Car'];
+      var query;
 
-  displayButtons();
+      displayButtons();
 
-  function displayButtons() {
-    $('#buttons').empty();
-    for (var i = 0; i < buttons.length; i++) {
-      var btn = $('<button>', {'id': buttons[i], 'type': 'button', 'class': 'btn btn-info buttons'}).text(buttons[i]);
-      $('#buttons').append(btn);
-    }
-  }
+      function displayButtons() {
+        $('#buttons').empty();
+        for (var i = 0; i < buttons.length; i++) {
+          var btn = $('<button>', {
+            'id': buttons[i],
+            'type': 'button',
+            'class': 'btn btn-info buttons'
+          }).text(buttons[i]);
+          $('#buttons').append(btn);
+        }
 
-  $(".buttons").on("click", function(event) {
-    event.preventDefault();
+        $('.buttons').on('click', function(event) {
+          event.preventDefault();
+          query = $(this).attr('id');
+          console.log(query);
+          displayGifs();
+        });
 
-      var query = $(this).attr('id');
-      var q = query.replace(' ', '+');
-      console.log('q', q);
-      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qkaEriFpJGD4wsNP9Um6v9eqkQd3puIU&limit=10&q=" + q;
-      $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).done(function(response) {
-            console.log(response);
-            $('#main-column').empty();
-            for (var i = 0; i < response.data.length; i++) {
-              var gifUrl = response.data[i].images.fixed_height.url;
-              console.log(query + ' gif');
-              var gifImage = $('<img>');
+      };
 
-              gifImage.attr('src', gifUrl);
-              gifImage.attr('alt', query + ' gif');
+      function displayGifs() {
+        var q = query.replace(' ', '+');
+        console.log('q', q);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qkaEriFpJGD4wsNP9Um6v9eqkQd3puIU&limit=10&q=" + q;
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+          console.log(response);
+          $('#main-column').empty();
+          for (var i = 0; i < response.data.length; i++) {
+            var gifUrl = response.data[i].images.fixed_height.url;
+            console.log(query + ' gif');
+            var gifImage = $('<img>');
 
-              $('#main-column').append(gifImage);
-            };
+            gifImage.attr('src', gifUrl);
+            gifImage.attr('alt', query + ' gif');
 
+            $('#main-column').append(gifImage);
+          }
+        });
+      };
+
+          $('#add').on('click', function(event) {
+            event.preventDefault();
+            var newBtn = $('#newButton').val().trim();
+            buttons.push(newBtn);
+            displayButtons();
           });
 
-    });
-
-  });
+        });
