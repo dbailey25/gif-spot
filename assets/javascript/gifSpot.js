18d8fef8b93992a9d2a00845875a4b8d44a3026a
dbailey25 <dtbailey25@gmail.com>
2018-01-08 13:35:28 -0500

    added ratings

--- assets/javascript/gifSpot.js
+++ assets/javascript/gifSpot.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
       // global variables
-      var buttons = ['Chevy Corvette', 'Chevy Camaro', 'Ford Mustang', 'Aston Martin', 'Koenigsegg', 'Nascar Race Car', 'Le Mans Race Car', 'Land Speed Race Car'];
+      var buttons = ['Chevy Corvette', 'Chevy Camaro', 'Ford Mustang', 'Aston Martin', 'Koenigsegg', 'Nascar Race Car', 'Le Mans Race Car', 'Rally Race Car'];
       var query;
       var returnLimit = 10;
       var gifStatic = [];
       var gifAnimated = [];
+      var gifRating = [];
       displayButtons();
 
       function displayButtons() {
@@ -17,31 +18,33 @@ $(document).ready(function() {
           }).text(buttons[i]);
           $('#buttons').append(btn);
         }
-
         $('.buttons').on('click', function(event) {
           event.preventDefault();
           query = $(this).attr('id');
           console.log(query);
           displayGifs();
         });
-
       };
-
-
 
       function generateGifs() {
         console.log('begin gif generation');
           $('#main-column').empty();
           for (var i = 0; i < gifStatic.length; i++) {
+            var gifDivId = 'gifDiv' + i;
+            var gifDiv = $('<div>', {'class': 'gifDiv', 'id': gifDivId})
+            $('#main-column').append(gifDiv);
+            var ratingDivId = 'ratingDiv' + i;
+            var ratingDiv = $('<div>', {'class': 'ratingDiv', 'id': ratingDivId});
+            $('#' + gifDivId).append(ratingDiv);
+            $('#' + ratingDivId).text('Rating: ' + gifRating[i]);
             var gifUrl_s = gifStatic[i];
             var gifImage = $('<img>');
-
             gifImage.attr('src', gifUrl_s);
             gifImage.attr('alt', query + ' gif');
             gifImage.attr('class', 'gifs');
             gifImage.attr('id', i);
             gifImage.attr('status', 'pause');
-            $('#main-column').append(gifImage);
+            $('#' + gifDivId).prepend(gifImage);
           }
 
         $('.gifs').on('click', function(event) {
@@ -59,8 +62,6 @@ $(document).ready(function() {
         });
       }
 
-
-
       function displayGifs() {
         var q = query.replace(' ', '+');
         console.log('q', q);
@@ -72,19 +73,19 @@ $(document).ready(function() {
           console.log(response);
           gifAnimated = [];
           gifStatic = [];
+          gifRating = [];
           for (var i = 0; i < response.data.length; i++) {
             var gifUrl_static = response.data[i].images.fixed_height_still.url;
             var gifUrl_animated = response.data[i].images.fixed_height.url;
+            var gif_rating = response.data[i].rating;
             gifAnimated.push(gifUrl_animated);
             gifStatic.push(gifUrl_static);
+            gifRating.push(gif_rating);
             console.log('response loop complete');
           }
             generateGifs();
         });
-
       };
-
-
 
           $('#add').on('click', function(event) {
             event.preventDefault();
@@ -92,5 +93,4 @@ $(document).ready(function() {
             buttons.push(newBtn);
             displayButtons();
           });
-
 });
